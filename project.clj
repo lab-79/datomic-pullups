b82a93447243d82f97425343f07c89553fb443b4
Trevor Hartman <trevorhartman@gmail.com>
2017-02-21 09:04:06 -0700

    Setup travis and codecov

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/lab-79/datomic-pullups"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:plugins [[lein-kibit "0.1.3"]]}}
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.7-SNAPSHOT"]
+                             [lein-kibit "0.1.3"]]}}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
