424d4ae6fde1e70174d68b700912b20ebfde0a03
Trevor Hartman <trevorhartman@gmail.com>
2017-02-21 09:27:19 -0700

    Run lein eastwood during build

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:plugins [[lein-cloverage "1.0.7-SNAPSHOT"]
+                             [jonase/eastwood "0.2.3"]
                              [lein-kibit "0.1.3"]]}}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
