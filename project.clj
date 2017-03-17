1dbbd739c7338d818de1426c90090d2314650259
Trevor Hartman <trevorhartman@gmail.com>
2017-03-16 20:05:02 -0600

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lab79/datomic-pullups "0.1.1"
+(defproject lab79/datomic-pullups "0.1.2-SNAPSHOT"
   :description "Union and intersection for Datomic pull syntax"
   :url "https://github.com/lab-79/datomic-pullups"
   :license {:name "Eclipse Public License"
