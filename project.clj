11e75eecf43ac5dd71f820d9dbd302fd5419ecd7
Brian Noguchi <brian.noguchi@gmail.com>
2017-08-07 17:34:55 -0700

    Update clojure, lein-cloverage, eastwood, lein-kibit

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/lab-79/datomic-pullups"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:plugins [[lein-cloverage "1.0.7-SNAPSHOT"]
-                             [jonase/eastwood "0.2.3"]
-                             [lein-kibit "0.1.3"]]}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.9"]
+                             [jonase/eastwood "0.2.4"]
+                             [lein-kibit "0.1.5"]]}}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]])
